172c1eb6dbe6157e1b92114fe342f665cfda6823
Souma <souma.mondal@protonmail.com>
2026-01-25 18:44:26 -0500

    adds App, Form, Header, TODOHero, TODOList components

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+// src/app/page.js
+
+import React from "react";
+import Form from "./Form";
+import Header from "./Header";
+import TODOHero from "./TODOHero";
+import TODOList from "./TODOList";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div className="wrapper">
+			<Header />
+			<TODOHero todos_completed={0} total_todos={0} />
+			<Form />
+			<TODOList todos={[]} />
+		</div>
+	);
 }
-
 export default App;
