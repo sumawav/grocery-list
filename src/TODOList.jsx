beb3c83db658a622cf9c07238cb11496468515a1
Souma <souma.mondal@protonmail.com>
2026-01-26 01:15:05 -0500

    adds ability to delete

--- src/TODOList.jsx
+++ src/TODOList.jsx
@@ -1,18 +1,30 @@
 // src/components/TODOList.jsx
 
 function TODOList({ todos, setTodos }) {
+	const handleDeleteTodo = (id) => {
+		const newTodos = todos.filter((todo) => todo.id !== id);
+		console.log(newTodos);
+		setTodos(newTodos);
+	};
 	return (
 		<ol className="todo_list">
 			{todos && todos.length > 0
 				? todos?.map((item, index) => (
-						<Item key={index} item={item} setTodos={setTodos} />
+						<Item
+							key={index}
+							item={item}
+							setTodos={setTodos}
+							handleDeleteTodo={handleDeleteTodo}
+						/>
 					))
 				: null}
 		</ol>
 	);
 }
 
-function Item({ item }) {
+function Item({ item, setTodos, handleDeleteTodo }) {
+
+	const handleDeleteClick = () => handleDeleteTodo(item.id);
 	return (
 		<li id={item?.id} className="todo_item">
 			<button className="todo_items_left">
@@ -22,7 +34,7 @@ function Item({ item }) {
 				<button>
 					<span className="visually-hidden">Edit</span>
 				</button>
-				<button>
+				<button onClick={handleDeleteClick}>
 					<span className="visually-hidden">Delete</span>
 				</button>
 			</div>
