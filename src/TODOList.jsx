d56ea050ef2b18023be2398bd05f6e823f470a40
Souma <souma.mondal@protonmail.com>
2026-01-28 16:06:32 -0800

    adds input for edit mode

--- src/TODOList.jsx
+++ src/TODOList.jsx
@@ -38,14 +38,36 @@ function Item({ item, setTodos, handleDeleteTodo }) {
 		setEditing((e) => !e);
 	};
 
+	const handleUpdateTodo = (event) => {
+		event.preventDefault();
+		const text = event.target.updatetodo.value;
+		console.log(text);
+	};
+
 	return (
 		<li id={item?.id} className="todo_item">
-			<button
-				onClick={() => handleToggleTodo(item.id)}
-				className="todo_items_left"
-			>
-				<p>{item?.title}</p>
-			</button>
+			{isEditing ? (
+				<form className="form" onSubmit={handleUpdateTodo}>
+					<label htmlFor="updatetodo">
+						<input
+							type="text"
+							name="updatetodo"
+							id="updatetodo"
+							placeholder="update todo"
+						/>
+					</label>
+					<button type="submit">
+						<span className="visually-hidden">Submit</span>
+					</button>
+				</form>
+			) : (
+				<button
+					onClick={() => handleToggleTodo(item.id)}
+					className="todo_items_left"
+				>
+					<p>{item?.title}</p>
+				</button>
+			)}
 			<div className="todo_items_right">
 				<span>{item.is_completed ? "COMPLETE" : ""}</span>
 				<button onClick={handleToggleEdit}>
